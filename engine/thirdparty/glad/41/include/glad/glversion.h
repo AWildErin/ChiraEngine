5b6ffe7fca14f4101c90d974a05867892af53267
craftablescience <me@craftablescience.info>
2022-09-28 14:56:09 -0400

    feat: allow the use of OpenGL 4.3 on non-mac platforms

--- engine/thirdparty/glad/41/include/glad/glversion.h
+++ engine/thirdparty/glad/41/include/glad/glversion.h
@@ -2,11 +2,7 @@
 
 #include <string_view>
 
-namespace chira {
-
-constexpr int GL_VERSION_MAJOR = 4;
-constexpr int GL_VERSION_MINOR = 1;
+constexpr const int GL_VERSION_MAJOR = 4;
+constexpr const int GL_VERSION_MINOR = 1;
 constexpr std::string_view GL_VERSION_STRING = "#version 410 core";
 constexpr std::string_view GL_VERSION_STRING_PRETTY = "4.1 Core";
-
-} // namespace chira
